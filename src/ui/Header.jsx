13c54f75f125b871e5124383cfda38eacb707715
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2023-07-20 22:41:43 +0300

    first commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Schnell Pizza Co.</Link>
+      <p>Oner</p>
+    </header>
+  );
+}
+
+export default Header;
